61193fbb28f9868d9134af456e6c5c4f78a23612
magoador <magomedov951.mail.ru>
2023-03-28 11:17:55 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/assets/img"));
+app.use(express.json());
+
+const { PORT, MONGO_SERVER } = process.env;
+
+const connectAndStartServer = async () => {
+  try {
+    await mongoose.connect(MONGO_SERVER);
+    app.listen(PORT, () => {
+      console.log(`Server started: http://localhost:${PORT}/`);
+    });
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+};
+
+connectAndStartServer();
