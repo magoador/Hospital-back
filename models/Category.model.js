8faa4aa0ac7e5ded111355b3dca60b708d577ab8
magoador <magomedov951.mail.ru>
2023-04-03 13:49:59 +0300

    реализовал вывод категорий с бэка

--- models/Category.model.js
+++ models/Category.model.js
@@ -1,9 +1,10 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const categorySchema = mongoose.Schema({
-    name: String
-})
+  image: String,
+  name: String,
+});
 
-const Category = mongoose.model('Category', categorySchema)
+const Category = mongoose.model("Category", categorySchema);
 
-module.exports = Category
+module.exports = Category;
